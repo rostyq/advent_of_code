f15252f79d4fbea69f9f1c768f20a05c3b448681
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2021-12-22 21:12:51 +0200

    2015: day 10

---
+++ 2015/day_10/solution.py
@@ -0,0 +1,33 @@
+from sys import argv
+
+
+def look_and_say(digits: str) -> str:
+    length = len(digits)
+    current = 0
+    result = ""
+
+    while current < length:
+        digit = digits[current]
+
+        count = 1
+        offset = current + 1
+        while offset < length:
+            if digit == digits[offset]:
+                count += 1
+                offset += 1
+            else:
+                break
+
+        result += f"{count}{digit}"
+
+        current = offset
+
+    return result
+
+
+result = argv[1]
+
+for _ in range(int(argv[2])):
+    result = look_and_say(result)
+
+print(len(result))
